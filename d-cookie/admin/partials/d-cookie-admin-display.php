eb70f631c5526b8e6df84630f342a3eda2f95548
jn199 <joshuaneve@Joshuas-MacBook-Pro-4.local>
2018-06-11 20:57:44 +0100

    Using Scaffolding For Initial Setup
    Preparations made simple using “Boilerplate” as scaffolding.

---
+++ d-cookie/admin/partials/d-cookie-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://github.com/jn199
+ * @since      1.0.0
+ *
+ * @package    D_Cookie
+ * @subpackage D_Cookie/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ d-cookie/public/partials/d-cookie-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://github.com/jn199
+ * @since      1.0.0
+ *
+ * @package    D_Cookie
+ * @subpackage D_Cookie/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
